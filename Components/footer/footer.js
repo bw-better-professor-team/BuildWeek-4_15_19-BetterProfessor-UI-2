b7c65c017c6e05c073ad44a9aa3f306e9b9a651b
Firealem Erko <firealemg@yahoo.com>
2019-04-17 16:05:57 -0400

    modified the script on the footer

--- Components/footer/footer.js
+++ Components/footer/footer.js
@@ -1,6 +1,6 @@
 const footer = document.querySelector(".footer");
 window.addEventListener("scroll", function() {
-  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight-100) {
     footer.classList.remove("fnon");
   } else {
     footer.classList.add("fnon");
