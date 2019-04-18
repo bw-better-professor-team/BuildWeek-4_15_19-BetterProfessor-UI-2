609ae1d07fef5b251a841e3576caba5207de02c8
Firealem Erko <firealemg@yahoo.com>
2019-04-18 16:03:32 -0400

    added comments

--- Components/footer/footer.js
+++ Components/footer/footer.js
@@ -1,3 +1,5 @@
+//This script hides the footer except when the user scrolls to the bottom of the page
+
 const footer = document.querySelector(".footer");
 window.addEventListener("scroll", function() {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight-100) {
